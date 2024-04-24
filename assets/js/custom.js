2b3d9a0becc8f6a2f9cc1fb7ad8c7ee804e1f1fd
Jeanne Endres <jeanne.endres@gmx.de>
2024-04-24 21:08:23 +0200

    add hero slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,6 +15,22 @@
     menu.classList.remove("open");
   });
 
+  /**
+   * Hero type effect
+   */
+  const typed = document.getElementsByClassName('typed')[0];
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
   // Close menu after click on smaller screens
   $(window).on("resize", function() {
     if ($(window).width() < 846) {
